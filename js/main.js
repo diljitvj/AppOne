929e54123e5859a2f23dc1ff2640e07b2c0e0618
Diljit Vj <diljitvj@gmail.com>
2016-08-29 17:26:16 +0530

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
+var userDetails = kendo.observable({
+    firstName : null,
+    lastName :null,
+    email:null,
+    picUrl:null,
+    getAnother : function(){
+        $.ajax({
+  url: 'https://randomuser.me/api/?inc=gender,name,email,picture',
+  dataType: 'json',
+  success: function(data) {
+
+    userDetails.set("firstName",data.results[0].name.first);
+    userDetails.set("lastName",data.results[0].name.last);
+    userDetails.set("email",data.results[0].email);
+    userDetails.set("picUrl",data.results[0].picture.large);
+
+    console.log(userDetails);
+                            }
+            });
+
+                            }
+});
+
+userDetails.getAnother();
+const kendoDiv = $("#appOneDiv");
+kendo.bind(kendoDiv,userDetails);
+
+
+
+
+
+//console.log(userDetails);
+
+/*
+
+var viewModel = kendo.observable({
+    name: "John Doe",
+    displayGreeting: function() {
+        var name = this.get("name");
+        alert("Hello, " + name + "!!!");
+    }
+});
+
+
+const kendoDiv = $("#appOneDiv")
+kendo.bind(kendoDiv,viewModel);*/
 
